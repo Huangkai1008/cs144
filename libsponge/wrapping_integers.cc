ccd75db2c43e6e9de8013595303a8ee574fa7326
huangkai <h1770360848@outlook.com>
2022-06-11 16:47:38 +0800

    feat(lab2): sequence numbers

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -6,17 +6,14 @@
 // automated checks run by `make check_lab2`.
 
 template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
+void DUMMY_CODE(Targs &&.../* unused */) {}
 
 using namespace std;
 
 //! Transform an "absolute" 64-bit sequence number (zero-indexed) into a WrappingInt32
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
-WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
-}
+WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) { return isn + static_cast<uint32_t>(n); }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
 //! \param n The relative sequence number
@@ -29,6 +26,13 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+    //! Get the min step between n and checkpoint.
+    //! In most cases, just add `step` to the `checkpoint` will get the absolute sequence number.
+    //! Sometimes, the absolute seq is negative, it should add another 2^32.
+    int32_t min_step = n - wrap(checkpoint, isn);
+    int64_t res = checkpoint + min_step;
+    if (res < 0) {
+        res += (1UL << 32);
+    }
+    return res;
 }
